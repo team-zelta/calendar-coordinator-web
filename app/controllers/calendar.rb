506402fd8ea1b0529bce828b729916a992e8c0a1
tienyulin <t.tienyulin@gmail.com>
2021-05-28 09:00:21 +0800

    refactor: adjust calendar list route

--- app/controllers/calendar.rb
+++ app/controllers/calendar.rb
@@ -7,15 +7,17 @@ module CalendarCoordinator
   # Web controller for CalendarCoordinator API
   class App < Roda
     route('calendar') do |routing|
-      routing.on 'list/group' do
-        # GET /calendar/list/group/{group_id}
+      routing.on 'group' do
         routing.on String do |group_id|
-          group_service = GroupService.new(App.config)
-          @calendar_list = group_service.owned_calendars(group_id: group_id)
+          # GET /calendar/group/{group_id}/list
+          routing.is 'list' do
+            group_service = GroupService.new(App.config)
+            @calendar_list = group_service.owned_calendars(group_id: group_id)
 
-          view 'calendar', locals: { calendar_list: @calendar_list }
-        rescue StandardError
-          view 'calendar'
+            view 'calendar', locals: { calendar_list: @calendar_list }
+          rescue StandardError
+            view 'calendar'
+          end
         end
       end
     end
